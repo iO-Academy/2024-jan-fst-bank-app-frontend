bcc3f3ca9ba081458fa24771121f49f453ea2bf2
Elizabeth Hartley <elizabeth.rae.gadsbygmail.com>
2024-05-14 15:50:17 +0100

    made text atoms, styled all atoms

--- src/Components/Atoms/ButtonAtom/index.tsx
+++ src/Components/Atoms/ButtonAtom/index.tsx
@@ -2,13 +2,16 @@ interface ButtonProps {
     value: string
     type?: 'submit' | 'reset' | 'button' | undefined
     onClick?: () => void
-    width?: string
+    margin?: string
+    //  margin is changeable, change as required by passing in as prop
 }
 
 function ButtonAtom(props: ButtonProps) {
-    const {value, type, onClick, width="28"} = props
+    const {value, type, onClick, margin} = props
     return  (
-        <button className={"border border-green-300 rounded-md w-" + width} type={type} onClick={onClick}>{value}</button>
+        <div className={"flex justify-center"}>
+        <button className={"border-1 border border-emerald-900 rounded-md p-2 bg-emerald-500 text-white m-" + margin}  type={type} onClick={onClick}>{value}</button>
+        </div>
     )
 }
 
--- src/Components/Molecules/DropdownMenuMolecule/index.tsx
+++ src/Components/Molecules/DropdownMenuMolecule/index.tsx
@@ -2,17 +2,21 @@ import DropdownOptionAtom from "../../Atoms/DropdownOptionAtom";
 
 interface DropdownProps {
     accounts: Array<string>
+    margin?: string
+//  margin is changeable, change as required by passing in as prop
 }
 
 function DropdownMolecule(props: DropdownProps) {
-    const {accounts} = props
+    const {accounts, margin} = props
     return (
         <>
+            <div className={"flex justify-center"}>
             <select name="" id=""
-                    className="rounded-md bg-emerald-500 border-1 border border-emerald-900 text-white p-2 m-2">
+                    className={"rounded-md bg-emerald-500 border-1 border border-emerald-900 text-white p-2 m-" + margin}>
                 <option selected disabled value="">Select an account</option>
                 {accounts.map(account => <DropdownOptionAtom accountOption={account} />)}
             </select>
+            </div>
         </>
     )
 }
