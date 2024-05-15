526864073d6a9b6fa6020fe1f034a6867a2bf136
Max Hamilton <maxeh987@gmail.com>
2024-05-15 15:15:40 +0100

    added routes in app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import RegisterPage from "./Components/Pages/RegisterPage";
 import IndexPage from "./Components/Pages/IndexPage";
 import HomePage from "./Components/Pages/HomePage";
+import AddFundsPage from "./Components/pages/AddFundsPage";
 import AccountBalanceAtom from "./Components/Atoms/AccountBalanceAtom";
+import AddAccountPage from "./Components/Pages/AddAccountPage";
+import AccountPage from "./Components/Pages/AccountPage";
 
 function App() {
 
@@ -14,6 +17,9 @@ function App() {
                     <Route key={"Register"} path={"/register"} element={<RegisterPage />}></Route>
                     <Route key={"Index"} path={"/"} element={<IndexPage />}></Route>
                     <Route key={"Home"} path={"/home"} element={<HomePage />}></Route>
+                    <Route key={"AddFunds"} path={"/add"} element={<AddFundsPage />}></Route>
+                    <Route key={"AddAccount"} path={"/addAccount"} element={<AddAccountPage />}></Route>
+                    <Route key={"Account"} path={"/account"} element={<AccountPage />}></Route>
                 </Routes>
             </BrowserRouter>
         </>
