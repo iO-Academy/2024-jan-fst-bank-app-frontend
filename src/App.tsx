efe08508d98c95acf7dd7f042abb5ad4c16c412f
John Harrison <john.harrison147@virginmedia.com>
2024-05-15 09:52:04 +0100

    added routing and some very basic pages to be added to

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
-
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import RegisterPage from "./Components/Pages/RegisterPage";
+import IndexPage from "./Components/Pages/IndexPage";
+import HomePage from "./Components/Pages/HomePage";
 
 function App() {
 
     return (
         <>
+            <BrowserRouter>
+                <Routes>
+                    <Route key={"Register"} path={"/register"} element={<RegisterPage />}></Route>
+                    <Route key={"Index"} path={"/"} element={<IndexPage />}></Route>
+                    <Route key={"Home"} path={"/home"} element={<HomePage />}></Route>
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
