4a7f529c334ed26266187728532f54fa52781cbf
Max Hamilton <maxeh987@gmail.com>
2024-05-16 13:45:34 +0100

    fixed some minor syntax issues

--- src/Components/Pages/AccountPage/index.tsx
+++ src/Components/Pages/AccountPage/index.tsx
@@ -2,7 +2,7 @@ import buttonAtom from "../../Atoms/ButtonAtom";
 import backButtonAtom from "../../Atoms/BackButtonAtom";
 
 function AccountPage() {
-    return <p>hello</p>;
+    return '';
 }
 
 export default AccountPage
--- src/Components/Pages/AddAccountPage/index.tsx
+++ src/Components/Pages/AddAccountPage/index.tsx
@@ -1,4 +1,3 @@
 import buttonAtom from "../../Atoms/ButtonAtom";
 import backButtonAtom from "../../Atoms/BackButtonAtom";
 
-function AddAccountPage
